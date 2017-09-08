fec12a5775e4f0dcacecfe88a4a2e3eff7bb95f2
Pan <panfree23@gmail.com>
2017-09-08 15:55:35 +0800

    Update url-loader config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -58,7 +58,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         exclude: [resolve('src/icons')],
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
