9788cdab23076b81aec308ccaa873dd6c61bc02f
Pan <panfree23@gmail.com>
2017-10-19 12:44:52 +0800

    fix: import MessageBox

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '../store'
 import { getToken } from '@/utils/auth'
 
