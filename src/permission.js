576e37762acb47ba1d7083006b1ed9d1a72579cd
Pan <panfree23@gmail.com>
2017-10-25 17:37:42 +0800

    fix: next to login when token is not right

--- src/permission.js
+++ src/permission.js
@@ -2,6 +2,7 @@ import router from './router'
 import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
 import 'nprogress/nprogress.css'// Progress 进度条样式
+import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
@@ -14,6 +15,11 @@ router.beforeEach((to, from, next) => {
       if (store.getters.roles.length === 0) {
         store.dispatch('GetInfo').then(res => { // 拉取用户信息
           next()
+        }).catch(() => {
+          store.dispatch('FedLogOut').then(() => {
+            Message.error('验证失败,请重新登录')
+            next({ path: '/login' })
+          })
         })
       } else {
         next()
