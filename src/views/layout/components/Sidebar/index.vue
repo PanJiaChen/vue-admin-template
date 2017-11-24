86096e4eabd6e442aef626db5e1a336504821e51
Pan <panfree23@gmail.com>
2017-11-24 16:30:40 +0800

    refactor:router&&sidemenu

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,23 +1,28 @@
 <template>
-  <el-menu mode="vertical" theme="dark" :default-active="$route.path">
-    <sidebar-item :routes="routes"></sidebar-item>
-  </el-menu>
+  <scroll-bar>
+    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
+      <sidebar-item :routes='routes'></sidebar-item>
+    </el-menu>
+  </scroll-bar>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
+import ScrollBar from '@/components/ScrollBar'
+
 export default {
-  components: { SidebarItem },
+  components: { SidebarItem, ScrollBar },
   computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
     routes() {
       return this.$router.options.routes
+    },
+    isCollapse() {
+      return !this.sidebar.opened
     }
   }
 }
 </script>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-.el-menu {
-  min-height: 100%;
-}
-</style>
