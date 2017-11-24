b97b370f0408b77d7da11e6a3e9faa2c1b56ada0
Pan <panfree23@gmail.com>
2017-11-24 17:21:32 +0800

    perf:format code

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -8,7 +8,7 @@
         <i class="el-icon-caret-bottom"></i>
       </div>
       <el-dropdown-menu class="user-dropdown" slot="dropdown">
-        <router-link class='inlineBlock' to="/">
+        <router-link class="inlineBlock" to="/">
           <el-dropdown-item>
             Home
           </el-dropdown-item>
