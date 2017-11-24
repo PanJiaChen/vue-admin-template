dcd3e316589e5d04e9286341110c672bfe3da2f0
Pan <panfree23@gmail.com>
2017-11-24 14:08:06 +0800

    refactor:refine SvgIcon

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import IconSvg from '@/components/Icon-svg'// svg组件
+import SvgIcon from '@/components/SvgIcon'// svg组件
 
 // register globally
-Vue.component('icon-svg', IconSvg)
+Vue.component('svg-icon', SvgIcon)
 
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 const req = require.context('./svg', false, /\.svg$/)
