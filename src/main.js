e3b4e103aa86d2b7bd79ffc315eb8c504f80bd47
Pan <panfree23@gmail.com>
2017-11-24 13:50:31 +0800

    refactor: update element-ui 2.0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
 import App from './App'
 import router from './router'
