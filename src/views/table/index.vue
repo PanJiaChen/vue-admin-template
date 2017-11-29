37085484d125c90940a0af695312ff11db92768f
Pan <panfree23@gmail.com>
2017-11-29 09:51:10 +0800

    typo:change to en

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-table :data="list" v-loading.body="listLoading" element-loading-text="拼命加载中" border fit highlight-current-row>
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
       <el-table-column align="center" label='ID' width="95">
         <template slot-scope="scope">
           {{scope.$index}}
