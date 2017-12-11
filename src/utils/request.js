8ab770e9dee0c0771213e0cf4b4e3aeccbbcc1d3
Pan <panfree23@gmail.com>
2017-12-11 13:39:58 +0800

    lint:format code

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 15000                  // 请求超时时间
+  timeout: 15000 // 请求超时时间
 })
 
 // request拦截器
--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -43,7 +43,7 @@ export default {
     },
     logout() {
       this.$store.dispatch('LogOut').then(() => {
-        location.reload()  // 为了重新实例化vue-router对象 避免bug
+        location.reload() // 为了重新实例化vue-router对象 避免bug
       })
     }
   }
