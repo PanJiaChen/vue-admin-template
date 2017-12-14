3ea895562655c133fb1a47ddc14557fc1905c808
Pan <panfree23@gmail.com>
2017-12-14 16:41:09 +0800

    perf

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
 import router from './router'
 import store from './store'
+
 import '@/icons' // icon
-import '@/permission' // 权限
+import '@/permission' // permission control
 
 Vue.use(ElementUI, { locale })
 
