b15cdeaa1ec1f52c7fb4704f8a3b6f596f0fd46d
Pan <panfree23@gmail.com>
2017-12-15 11:39:43 +0800

    set allChunks:false

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -50,9 +50,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: utils.assetsPath('css/[name].[contenthash].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
+      allChunks: false,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
