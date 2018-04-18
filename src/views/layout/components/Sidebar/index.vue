74bb623d681940dc5a928a8d30da77071fcf8706
Pan <panfree23@gmail.com>
2018-04-18 14:47:20 +0800

    update element-ui

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,6 +1,15 @@
 <template>
   <scroll-bar>
-    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
+    <el-menu
+      mode="vertical"
+      :collapse-transition="false"
+      :show-timeout="200"
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      background-color="#304156"
+      text-color="#bfcbd9"
+      active-text-color="#409EFF"
+    >
       <sidebar-item :routes="routes"></sidebar-item>
     </el-menu>
   </scroll-bar>
