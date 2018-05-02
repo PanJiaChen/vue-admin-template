45ebf11faba9d6aa405e85745e69b23eda859a12
Pan <panfree23@gmail.com>
2018-05-02 17:44:35 +0800

    fix typo

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -44,7 +44,7 @@ const user = {
     GetInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
         getInfo(state.token).then(response => {
-          const data = response
+          const data = response.data
           if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
             commit('SET_ROLES', data.roles)
           } else {
