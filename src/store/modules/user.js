409a0ab9d2fd43a9ca56c249e6ec960857ed098d
Pan <panfree23@gmail.com>
2018-05-02 17:38:00 +0800

    perf[permission]: add the verification of roles

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -44,8 +44,12 @@ const user = {
     GetInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
         getInfo(state.token).then(response => {
-          const data = response.data
-          commit('SET_ROLES', data.roles)
+          const data = response
+          if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
+            commit('SET_ROLES', data.roles)
+          } else {
+            reject('getInfo: roles must be a non-null array !')
+          }
           commit('SET_NAME', data.name)
           commit('SET_AVATAR', data.avatar)
           resolve(response)
