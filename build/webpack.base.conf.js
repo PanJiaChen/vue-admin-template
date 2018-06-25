dd37f247f829a9b81e9c1e947c8a13071d5959c3
Pan <panfree23@gmail.com>
2018-06-25 11:15:02 +0800

    chore: use Runtime-only

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,8 +34,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
--- src/main.js
+++ src/main.js
@@ -23,6 +23,5 @@ new Vue({
   el: '#app',
   router,
   store,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
