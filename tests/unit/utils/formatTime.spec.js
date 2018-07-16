23d2043d395c95ec2811459072b36d07b9e4ced0
Pan <panfree23@gmail.com>
2018-07-16 23:24:33 +0800

    update

---
+++ tests/unit/utils/formatTime.spec.js
@@ -0,0 +1,31 @@
+import { formatTime } from '@/utils/index.js'
+
+describe('Utils:formatTime', () => {
+  const d = 1531475641067 // "2018-07-13 17:54:01"
+
+  it('test now', () => {
+    expect(formatTime(+new Date() - 1)).toBe('刚刚')
+  })
+
+  it('less two minute', () => {
+    expect(formatTime(+new Date() - 60 * 2 * 1000)).toBe('2分钟前')
+  })
+
+  it('less two hour', () => {
+    expect(formatTime(+new Date() - 60 * 60 * 2 * 1000)).toBe('2小时前')
+  })
+
+  it('less one day', () => {
+    expect(formatTime(+new Date() - 60 * 60 * 24 * 1 * 1000)).toBe('1天前')
+  })
+
+  it('more than one day', () => {
+    expect(formatTime(d)).toBe('7月13日17时54分')
+  })
+
+  it('format', () => {
+    expect(formatTime(d, '{y}-{m}-{d} {h}:{i}')).toBe('2018-07-13 17:54')
+    expect(formatTime(d, '{y}-{m}-{d}')).toBe('2018-07-13')
+    expect(formatTime(d, '{y}/{m}/{d} {h}-{i}')).toBe('2018/07/13 17-54')
+  })
+})
