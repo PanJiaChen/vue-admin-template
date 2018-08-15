ba71a4b5bc3afe5f0706ab0a87e5974c35363334
Pan <panfree23@gmail.com>
2018-08-15 16:48:45 +0800

    typo

--- config/index.js
+++ config/index.js
@@ -66,7 +66,7 @@ module.exports = {
 
     productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'ource-map',
+    devtool: 'source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
