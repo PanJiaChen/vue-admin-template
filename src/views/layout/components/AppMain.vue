a91f687c2dcb9add0861943c910d739c8d515e66
Pan <panfree23@gmail.com>
2018-08-28 18:30:36 +0800

    perf[css]: refine fade-transform

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="app-main">
-    <transition name="fade" mode="out-in">
+    <transition name="fade-transform" mode="out-in">
+      <!-- or name="fade" -->
       <!-- <router-view :key="key"></router-view> -->
       <router-view/>
     </transition>
