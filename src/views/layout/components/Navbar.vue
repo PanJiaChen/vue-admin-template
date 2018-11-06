6df4a679bfecea24a26dd09d03b51037c5188b99
Pan <panfree23@gmail.com>
2018-11-06 16:18:14 +0800

    perf[css]: refine navbar css

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -76,6 +76,7 @@ export default {
       cursor: pointer;
       margin-top: 5px;
       position: relative;
+      line-height: initial;
       .user-avatar {
         width: 40px;
         height: 40px;
