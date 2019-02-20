1052ab5bf884171f5c34dc34b3e9b86e39b65c34
Pan <panfree23@gmail.com>
2019-02-20 13:31:27 +0800

    perf: add NProgress configuration

--- src/permission.js
+++ src/permission.js
@@ -1,9 +1,11 @@
 import router from './router'
 import store from './store'
-import NProgress from 'nprogress' // Progress 进度条
-import 'nprogress/nprogress.css'// Progress 进度条样式
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
 import { Message } from 'element-ui'
-import { getToken } from '@/utils/auth' // 验权
+import { getToken } from '@/utils/auth' // getToken from cookie
+
+NProgress.configure({ showSpinner: false })// NProgress configuration
 
 const whiteList = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
