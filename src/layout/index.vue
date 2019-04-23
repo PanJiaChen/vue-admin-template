b2fe1a22e14e9d7055ca43b926a994e27553eeb4
PLAsusu <35716280+plasusu@users.noreply.github.com>
2019-04-23 09:50:47 +0800

    fix: fixed error action name (#339)

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -44,7 +44,7 @@ export default {
   },
   methods: {
     handleClickOutside() {
-      this.$store.dispatch('CloseSideBar', { withoutAnimation: false })
+      this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
     }
   }
 }
