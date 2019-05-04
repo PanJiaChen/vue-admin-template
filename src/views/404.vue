e552d3b5d118fc631609268498b9189791dda72b
zhouliang <liang.zhou@innvote.com>
2019-05-04 16:36:22 +0800

    translate 404 page to Chinese

--- src/views/404.vue
+++ src/views/404.vue
@@ -9,12 +9,9 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">All rights reserved
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a>
-        </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-        <a href="" class="bullshit__return-home">Back to home</a>
+        <div class="bullshit__info">{{ tips }}</div>
+        <a href="" class="bullshit__return-home">回到主页</a>
       </div>
     </div>
   </div>
@@ -26,7 +23,10 @@ export default {
   name: 'Page404',
   computed: {
     message() {
-      return 'The webmaster said that you can not enter this page...'
+      return '你要找的页面不存在...'
+    },
+    tips() {
+      return '请检查输入的地址是否正确，或者点击按钮回到主页'
     }
   }
 }
