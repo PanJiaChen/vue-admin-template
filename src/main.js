ac4865ab2d609668ca27f85e8fd20fb8cb71f278
Pan <panfree23@gmail.com>
2019-05-13 12:56:39 +0800

    chore: use mockjs in production environment

--- src/main.js
+++ src/main.js
@@ -17,12 +17,16 @@ import '@/permission' // permission control
 
 /**
  * If you don't want to use mock-server
- * you want to use mockjs for request interception
- * you can execute:
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
  *
- * import { mockXHR } from '../mock'
- * mockXHR()
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
  */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
