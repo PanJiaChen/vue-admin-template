73c9d45ed61bf23ac9ca106b1c7216c4eaae2726
Stephy Cat <stephycat@link.cuhk.edu.hk>
2019-05-31 10:03:20 +0000

    use process.env.PORT to allow dynamic port

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
 // For example, Mac: sudo npm run
-const port = 9528 // dev port
+const port = process.env.PORT || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
