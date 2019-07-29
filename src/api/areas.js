acaef35d73567e9a0b17d943c7830e86296364ab
Jayamal <jayamaljayamaha2@gmail.com>
2019-07-29 20:23:00 +0530

    added axios api calls

---
+++ src/api/areas.js
@@ -0,0 +1,103 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+Vue.prototype.$http = axios
+
+export function getAreasList() {
+  const url = 'https://netapi.danderdee.com/api/areas'
+  this.$http.get(url, {
+    headers: {
+      Accept: 'application/json'
+    }
+  }).then(result => {
+    return result.data
+  }).catch(error => {
+    return 'error: ' + error
+  })
+}
+
+export function createAnArea(postCode) {
+  const url = 'https://netapi.danderdee.com/api/areas'
+  const pyaload = {
+    postCode: postCode
+  }
+  this.$http.post(url, pyaload, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(result => {
+    return result.data
+  }).catch(error => {
+    return 'error' + error
+  })
+}
+
+export function aproveAnArea(id) {
+  const url = 'https://netapi.danderdee.com/api/areas/approve'
+  const payload = {
+    id: id
+  }
+  this.$http.post(url, payload, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(result => {
+    return result.data
+  }).catch(error => {
+    return 'error' + error
+  })
+}
+
+export function disaproveAnArea(id) {
+  const url = 'https://netapi.danderdee.com/api/areas/disapprove'
+  const payload = {
+    id: id
+  }
+  this.$http.post(url, payload, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(result => {
+    return result.data
+  }).catch(error => {
+    return 'error' + error
+  })
+}
+
+export function addWikiContent(id) {
+  const url = 'https://netapi.danderdee.com/api/areas/addwiki'
+  const payload = {
+    id: id
+  }
+  this.$http.post(url, payload, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(result => {
+    return result.data
+  }).catch(error => {
+    return 'error' + error
+  })
+}
+
+export function changeWikiContent(id, info) {
+  const url = 'https://netapi.danderdee.com/api/areas/changewiki'
+  const payload = {
+    id: id,
+    info: info
+  }
+  this.$http.post(url, payload, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(result => {
+    return result.data
+  }).catch(error => {
+    return 'error' + error
+  })
+}
