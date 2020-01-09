e345d514a98a12028f55e98d2d000305c609c85d
花裤衩 <panfree23@gmail.com>
2020-01-09 20:51:42 +0800

    fix: fixed mock server

--- mock/user.js
+++ mock/user.js
@@ -26,7 +26,7 @@ const users = {
 export default [
   // user login
   {
-    url: '/user/login',
+    url: '/vue-admin-template/user/login',
     type: 'post',
     response: config => {
       const { username } = config.body
@@ -49,7 +49,7 @@ export default [
 
   // get user info
   {
-    url: '/user/info\.*',
+    url: '/vue-admin-template/user/info\.*',
     type: 'get',
     response: config => {
       const { token } = config.query
@@ -72,7 +72,7 @@ export default [
 
   // user logout
   {
-    url: '/user/logout',
+    url: '/vue-admin-template/user/logout',
     type: 'post',
     response: _ => {
       return {
