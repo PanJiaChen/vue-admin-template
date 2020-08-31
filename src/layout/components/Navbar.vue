7dad4cb845dd03bd2fd4c7ab95f8e29ede387b39
Haniel Cui <coldice945@hotmail.com>
2020-08-31 15:44:19 +0800

    navbar right menu add class name

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -5,7 +5,7 @@
     <breadcrumb class="breadcrumb-container" />
 
     <div class="right-menu">
-      <el-dropdown class="avatar-container" trigger="click">
+      <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
           <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
           <i class="el-icon-caret-bottom" />
