15864546984b3ceb308267071660dc9eb99eaab0
w_lierxing <15510223171@163.com>
2021-12-16 17:13:39 +0800

    绘制折线图

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,7 +2,9 @@
   <div class="dashboard-container">
     <el-row>
       <el-col :span="24">
-        <VeLine :data="chartData" />
+        <div class="chart-container container-height">
+          <VeLine :data="chartData" :height="lineChartFirstHeight" :legend="lineChartLegend" :grid="lineChartGrid" :extend="lineChartExtend" :settings="lineChartSettings" :y-axis="lineChartyAxis" />
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -18,16 +20,80 @@ export default {
     VeLine
   },
   data() {
+    this.lineChartyAxis = {
+      splitLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: ['#EAEAEA']
+        }
+      },
+      axisLabel: {
+        show: true,
+        color: '#6F6F6F'
+      },
+      axisLine: {
+        lineStyle: {
+          color: '#D6D6D6'
+        },
+        show: true
+      },
+      axisTick: {
+        show: false
+      }
+    }
+    this.lineChartSettings = {
+      itemStyle: {
+        color: ({ seriesIndex }) => {
+          return ['#FFBE5A', '#64D7B9'][seriesIndex]
+        }
+        // color: 'red', // ({ dataIndex }) => ['#FFBE5A', '#64D7B9'][dataIndex],
+      }
+    }
+    this.lineChartExtend = {
+      xAxis: {
+        show: true,
+        axisLabel: {
+          interval: 0,
+          show: true,
+          color: '#6F6F6F'
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#D6D6D6'
+          },
+          show: true
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      series: (data) =>
+        data.map((item, i) => ({ ...item, itemStyle: { color: ['#FFBE5A', '#64D7B9', '#5587F0'][i] } })),
+    }
+    this.lineChartGrid = {
+      left: 18,
+      top: 16,
+      right: 18,
+      bottom: 42
+    }
+    this.lineChartLegend = {
+      bottom: 12,
+      left: 'center',
+      textStyle: {
+        color: '#6F6F6F'
+      }
+    }
     return {
+      lineChartFirstHeight: '206px',
       chartData: {
-        columns: ['日期', '访问用户', '下单用户', '下单率'],
+        columns: ['日期', 'UV', 'PV'],
         rows: [
-          { 日期: '1/1', 访问用户: 1393, 下单用户: 1093, 下单率: 0.32 },
-          { 日期: '1/2', 访问用户: 3530, 下单用户: 3230, 下单率: 0.26 },
-          { 日期: '1/3', 访问用户: 2923, 下单用户: 2623, 下单率: 0.76 },
-          { 日期: '1/4', 访问用户: 1723, 下单用户: 1423, 下单率: 0.49 },
-          { 日期: '1/5', 访问用户: 3792, 下单用户: 3492, 下单率: 0.323 },
-          { 日期: '1/6', 访问用户: 4593, 下单用户: 4293, 下单率: 0.78 }
+          { 日期: '第一周', UV: 1393, PV: 1093 },
+          { 日期: '第二周', UV: 3530, PV: 3230 },
+          { 日期: '第三周', UV: 2923, PV: 2623 },
+          { 日期: '第四周', UV: 1723, PV: 1423 },
+          { 日期: '第五周', UV: 3792, PV: 3492 },
+          { 日期: '第六周', UV: 4593, PV: 4293 }
         ]
       }
     }
@@ -42,10 +108,12 @@ export default {
 .dashboard {
   &-container {
     margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
+    .chart-container {
+      width: 100%;
+    }
+    .container-height {
+      height: 206px;
+    }
   }
 }
 </style>
