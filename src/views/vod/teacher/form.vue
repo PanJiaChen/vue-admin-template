8e29d1443f241fe005b74d63e0ceef7fafcb223e
mingxin_liao <2664500322@qq.com>
2022-11-18 21:36:03 +0800

    53头像上传有bug

--- src/views/vod/teacher/form.vue
+++ src/views/vod/teacher/form.vue
@@ -30,6 +30,16 @@
 
       <!-- 讲师头像 -->
       <el-form-item label="讲师头像">
+        <el-upload
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :on-error="handleAvatarError"
+          :action="BASE_API+'/admin/vod/file/upload'"
+          class="avatar-uploader">
+          <img v-if="teacher.avatar" :src="teacher.avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"/>
+        </el-upload>
       </el-form-item>
 
       <el-form-item>
@@ -61,6 +71,41 @@ export default {
     }
   },
   methods: {
+    // 上传成功回调
+    handleAvatarSuccess(res, file) {
+      console.log(res)
+      if (res.code==20000) {
+        console.log(res)
+        this.teacher.avatar = res.data
+        // 强制重新渲染
+        this.$forceUpdate()
+      } else {
+        this.$message.error('上传失败 （非0）')
+      }
+    },
+
+    // 错误处理
+    handleAvatarError() {
+      console.log('error')
+      debugger
+      this.$message.error('上传失败（http失败）')
+    },
+
+    // 上传校验
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+      debugger
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+
+    //根据id回显数据
     fetchDataById(id){
       teacherApi.getById(id).then(response => {
 		    this.teacher = response.data
